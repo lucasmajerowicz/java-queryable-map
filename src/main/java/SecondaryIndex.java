b896322f7ab951746ac922e568006d1ac2ee500c
Lucas Majerowicz <lucasmajerowicz@users.noreply.github.com>
2019-12-09 11:05:43 +0000

    Update SecondaryIndex.java

--- src/main/java/SecondaryIndex.java
+++ src/main/java/SecondaryIndex.java
@@ -2,36 +2,28 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SecondaryIndex<K, V> {
-    private Map<K, Set<V>> map = new ConcurrentHashMap<>();
-    private Map<V, Set<K>> mapReverse = new ConcurrentHashMap<>();
+    private Map<K, Collection<V>> map = new ConcurrentHashMap<>();
 
     public void put(K k, V v) {
-        Set<V> vs = map.computeIfAbsent(k, k1 -> new HashSet<>());
+        Collection<V> vs = map.computeIfAbsent(k, k1 -> new HashSet<>());
         vs.add(v);
-
-        Set<K> keys = mapReverse.computeIfAbsent(v, k1 -> new HashSet<>());
-        keys.add(k);
     }
 
     public Collection<V> get(K k) {
         return map.getOrDefault(k, Collections.EMPTY_SET);
     }
 
-    public void removeElement(V v) {
-        Set<K> keys = mapReverse.remove(v);
-        if (keys != null) {
-            keys.forEach(k -> {
-                Set<V> vs = map.get(k);
-                vs.remove(v);
-                if (vs.isEmpty()) {
-                    map.remove(k);
-                }
-            });
+    public void remove(K key, V value) {
+        Collection<V> values = map.get(key);
+        if (values != null) {
+            values.remove(value);
+            if (values.isEmpty()) {
+                map.remove(key);
+            }
         }
     }
 
     public void clear() {
         map.clear();
-        mapReverse.clear();
     }
 }
